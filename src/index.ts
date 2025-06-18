06c198b97f0862b15ab81eeed99420fee242bb6c
ICao Vy <icaovy2001@gmail.com>
2025-06-18 23:51:12 +0700

    UPDATE:: cors

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: [process.env.FE_ORIGIN_URL!, process.env.WEBSOCKET_URL!],
         methods: ["GET", "POST"],
     },
 });
